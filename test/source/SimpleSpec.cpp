edc5b72f3af2892377e480ee09af6157c5f8ea15
Michael England <mg.england@gmail.com>
2014-11-30 19:01:32 +0100

    Make build less verbose

--- test/source/SimpleSpec.cpp
+++ test/source/SimpleSpec.cpp
@@ -1,10 +1,10 @@
-#include <muffin/api/simple.hpp>
+#include <muffin/api/Simple.hpp>
 
 #include <catch.hpp>
 
 SCENARIO("Simple", "[example]") {
     GIVEN("A simple object") {
-        muffin::api::simple simple;
+        muffin::api::Simple simple;
         WHEN("asked") {
             THEN("it gives us the answer") {
                 REQUIRE(simple.theAnswer() == 42);
